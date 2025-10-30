6ff1a80ac966c26f4757b6991bdc13f8fe6dd99d
Medwin <medwin4belitung@gmail.com>
2025-10-30 16:12:40 +0700

    perubahan webpack.common.js lagi

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     publicPath: 'https://edwinguna.github.io/citycareapp-1/',
   },
   module: {
